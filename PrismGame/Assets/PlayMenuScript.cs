56786e6acb984024b103f20d3b1c59bc870270a9
Philipp Thalhammer <phips@Philipps-MBP.fritz.box>
2020-06-29 17:04:53 +0200

    First Menu GUI implemented and Repository set

---
+++ PrismGame/Assets/PlayMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMenuScript : MonoBehaviour
+{
+    public void PlayLow ()
+    {
+        SceneManager.LoadScene("Low");
+    }
+
+    public void PlayMedium ()
+    {
+        SceneManager.LoadScene("Medium");
+    }
+
+    public void PlayHigh ()
+    {
+        SceneManager.LoadScene("High");
+    }
+}
