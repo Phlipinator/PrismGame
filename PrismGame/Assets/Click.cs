5051a7866da8b43e23a1ca5ee1a561521afccbe5
Philipp Thalhammer <phips@Philipps-MBP.fritz.box>
2020-07-02 18:53:12 +0200

    Versuch MouseInput zu lesen ist fehlgeschlagen

---
+++ PrismGame/Assets/Click.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    //[SerializableField]
+    public LayerMask clickablesLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo, 100f, clickablesLayer.value))
+            {
+                hitInfo.collider.GetComponent<ClickOn>().ClickMe();
+            }
+
+            /*RaycastHit rayHit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, clickablesLayer))
+            {
+                rayHit.collider.GetComponent<ClickOn>().ClickMe();
+            }*/
+        }
+    }
+}
---
+++ PrismGame/Assets/ClickOn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOn : MonoBehaviour
+{
+    [SerializeField]
+    private Material red;
+    [SerializeField]
+    private Material green;
+
+    private SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //myRenderer = GetComponent<SpriteRenderer>();
+
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void ClickMe()
+    {
+        sprite.color = new Color(1, 0, 0, 1);
+        //myRenderer.material = green;
+    }
+
+}
