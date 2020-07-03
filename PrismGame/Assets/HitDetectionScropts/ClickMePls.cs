d59bd03fbd2aac9c1d98a03201025a0fb45753e0
Philipp Thalhammer <phips@Philipps-MBP.fritz.box>
2020-07-03 15:25:18 +0200

    Hit detection for the prisms is added, they change color if you click them

---
+++ PrismGame/Assets/HitDetectionScropts/ClickMePls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMePls : MonoBehaviour
+{
+
+    bool clicked = false;
+
+    void OnMouseDown()
+    {
+            if (!clicked)
+            {
+                this.GetComponent<Renderer>().material.color = Color.green;
+                clicked = true;
+
+                //The rotation part does not work jet
+                if (Input.GetKey(KeyCode.A))
+                {
+                transform.Rotate(Vector3.right * 30 * Time.deltaTime);
+            }
+            }
+
+            else
+            {
+                this.GetComponent<Renderer>().material.color = Color.red;
+                clicked = false;
+            }
+       
+    }
+
+    /*
+
+    //This Part should turn the Prism Red again when there is a click anywhere else on the screen
+    //but it does not work..
+   
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            {
+            if (clicked)
+            {
+                this.GetComponent<Renderer>().material.color = Color.red;
+                clicked = false;
+            }
+        }
+    }
+    */
+}
