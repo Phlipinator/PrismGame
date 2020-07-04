0edfaf920561bf22a1a497c3197aa02c1df5df58
Philipp Thalhammer <phips@Philipps-MBP.fritz.box>
2020-07-04 03:27:29 +0200

    Laser Implementation
    Laser is implemented but only works partially (I don't fully understand the lineRenderer)

--- PrismGame/Assets/Laser.cs
+++ PrismGame/Assets/Laser.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
 
-    private LineRenderer lr;
-    void Start()
+    private LineRenderer lineRenderer;
+    public Transform LaserHit;
+
+    private void Start()
     {
-        lr = GetComponent<LineRenderer>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = true;
+        lineRenderer.useWorldSpace = true;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        lr.SetPosition(0, transform.position);
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
-        {
-            if (hit.collider)
-            {
-                lr.SetPosition(1, hit.point);
-            }
-        }
-        else lr.SetPosition(1, transform.forward * 5000);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+        LaserHit.position = hit.point;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, LaserHit.position);
+
+        Debug.DrawLine(transform.position, hit.point);
     }
 }
