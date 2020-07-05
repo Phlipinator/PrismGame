92d3e47662df2c062aa9a75ffa718e1b4d13e4c1
Philipp Thalhammer <phips@Philipps-MBP.fritz.box>
2020-07-05 22:35:44 +0200

    Movable Laser is implemented, Hitdetection jet to come
    The Script Laser3 contains the code for moveable Lasers, Hit Dtetection is not implemented jet

---
+++ PrismGame/Assets/DrawLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lineGeneratorPrefab;
+    [SerializeField]
+    private GameObject linePointPrefab;
+    [SerializeField]
+    private GameObject Spawn;
+    [SerializeField]
+    private GameObject Target;
+
+
+    private void Start()
+    {
+        SpawnLineGenerator();
+    }
+
+    private void SpawnLineGenerator()
+    {
+        Vector3 SpawnPosition = Spawn.transform.position;
+        Vector3 TargetPosition = Target.transform.position;
+
+        GameObject newLineGen = Instantiate(lineGeneratorPrefab);
+        LineRenderer lRend = newLineGen.GetComponent<LineRenderer>();
+
+        lRend.SetPosition(0, SpawnPosition);
+        lRend.SetPosition(1, TargetPosition); 
+    }
+
+}
---
+++ PrismGame/Assets/Laser3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser3 : MonoBehaviour
+{
+    public float laserBeamLength;
+    private LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 endPosition = transform.position + (transform.right * laserBeamLength);
+        lineRenderer.SetPositions(new Vector3[] { transform.position, endPosition });
+    }
+}
